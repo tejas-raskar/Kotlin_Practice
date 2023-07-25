67d94604a17477139cc34f257ab422ab61a0cb19
Tejas Raskar <tjsrskr@gmail.com>
2023-07-25 12:24:27 +0530

    Added the logic for the 'Next' & 'Previous' buttons. Fixed some bugs in the UI

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,15 +8,23 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,16 +49,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppLayout() {
+
+    var pageNumber by remember{ mutableStateOf(0) }
+
+    val artworkToDisplay = when(pageNumber) {
+        0 -> R.drawable.artwork1
+        1 -> R.drawable.artwork2
+        else -> R.drawable.artwork3
+    }
+
+    val artworkName = when(pageNumber) {
+        0 -> "Trees during day"
+        1 -> "Deer In The Middle Of The Forest"
+        else -> "Crop person with leaf in forest"
+    }
+
+    val artworkYear = when(pageNumber) {
+        0 -> 2020
+        1 -> 2019
+        else -> 2019
+    }
+
+    val artistName = when(pageNumber) {
+        0 -> "Lukas Rodriguez"
+        1 -> "Zak Bentley"
+        else -> "Lil Artsy"
+    }
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
-        ArtworkDisplay(R.drawable.artwork)
+        ArtworkDisplay(artworkToDisplay)
 
-        ArtworkNameAndArtist(artworkName = "Artwork", artistName = "Artist", year = 1999)
+        ArtworkNameAndArtist(artworkName = artworkName, artistName = artistName, year = artworkYear)
 
         //Prev and Next Buttons
         Row(
@@ -60,10 +95,10 @@ fun AppLayout() {
                 .wrapContentHeight(Alignment.Bottom),
             horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
-            Button(onClick = {}) {
+            Button(onClick = {pageNumber--}) {
                 Text(text = "Previous")
             }
-            Button(onClick = {}) {
+            Button(onClick = {pageNumber++}) {
                 Text(text = "Next")
             }
         }
@@ -77,13 +112,13 @@ fun ArtworkDisplay(@DrawableRes artwork: Int) {
     Surface(
         modifier = Modifier
             .padding(32.dp)
-            .padding(top = 32.dp, bottom = 32.dp),
+            .padding(top = 16.dp, bottom = 16.dp),
         shadowElevation = 20.dp,
     ) {
         Image(
             painter = painterResource(artwork),
             contentDescription = null,
-            modifier = Modifier.padding(32.dp)
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
@@ -110,8 +145,9 @@ fun ArtworkNameAndArtist(
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Light
             )
+            Spacer(modifier = Modifier.height(15.dp))
             Text(
-                text = "$artistName($year)",
+                text = "$artistName ($year)",
                 fontWeight = FontWeight.Bold,
                 fontSize = 15.sp
             )
